57f55ccb01c2c7252b8ff8b5e57c518dd0d1dabc
Matthias Losch <mail@matthiaslosch.de>
2021-11-15 12:54:36 +0100

    Add options for naming the images and choosing an image format
    Let the user choose a naming scheme with -n/--name and an image format
    with -f/--format.
    
    I don't know if it's a good idea to choose a format other than PNG
    though, especially JPEG. I suspect the result will be too blurred to get
    back the original file.

--- imagify.sh
+++ imagify.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-if [[ ! -e $1 ]]; then
-    echo "Usage: $0 FILE"
+positional=()
+image_name_prefix="img_"
+format="png"
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        -n|--name)
+            image_name_prefix="$2"
+            shift # past argument.
+            shift # past value.
+            ;;
+        -f|--format)
+            format="$2"
+            shift # past argument.
+            shift # past value.
+            ;;
+        *) # unknown option
+            positional+=("$1")
+            shift # past argument
+            ;;
+    esac
+done
+
+if [ -z "$positional" ]; then
+    echo "Usage: $0 <--name NAME> <--format FORMAT> FILE"
     exit 1
 fi
 
-binary_size=$(stat -c %s $1)
+binary_size=$(stat -c %s ${positional[0]})
 
 # Image dimesions: 512 x 512.
 bytes_per_image=$((262144))
@@ -13,16 +37,16 @@ number_of_images=$((binary_size / bytes_per_image))
 remainder=$((binary_size % bytes_per_image))
 
 for (( i = 0; i < $number_of_images; ++i )); do
-    dd bs=512 if=$1 skip=$((512 * i)) count=512 | convert -depth 8 -size 512x512 -format GRAY GRAY:- "img_${i}.png"
+    dd bs=512 if=${positional[0]} skip=$((512 * i)) count=512 | convert -depth 8 -size 512x512 -format GRAY GRAY:- "${image_name_prefix}${i}.${format}"
 done
 
 if [[ $remainder > 0 ]]; then
-    dd bs=512 if=$1 skip=$((512 * number_of_images)) count=512 if=$1 of=imagify_temp
+    dd bs=512 if=${positional[0]} skip=$((512 * number_of_images)) count=512 if=${positional[0]} of=imagify_temp
 
     # Fill up the rest of the file with null values.
     temp_size=$(stat -c %s imagify_temp)
     dd if=/dev/zero bs=1 count=$((bytes_per_image - temp_size)) >> imagify_temp
 
-    convert -depth 8 -size 512x512 -format GRAY GRAY:imagify_temp "img_${number_of_images}.png"
+    convert -depth 8 -size 512x512 -format GRAY GRAY:imagify_temp "${image_name_prefix}${number_of_images}.${format}"
     rm imagify_temp
 fi
